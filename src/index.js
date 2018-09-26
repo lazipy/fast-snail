0e76057afe9796696499d049fe7c03fada7cea9c
ZIPING LI <ziping@oriente.com>
2018-09-26 18:14:49 +0800

    init:初始化组件开发基础目录

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-const test = () => {
-  // console.log(111)
+import Popper from './packages/popper/main';
+import Scroller from './packages/scroller/main';
+
+const components = [
+  Popper,
+  Scroller
+];
+
+const install = (Vue) => {
+  if (install.installed) return;
+  components.map(component => {
+    Vue.component(component.name, component);
+  });
+
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
 }
 
-export default test;
+export default {
+  install,
+  Popper,
+  Scroller
+};
---
+++ src/packages/popper/main.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'vPopper'
+};
+</script>
+
+<style>
+
+</style>
