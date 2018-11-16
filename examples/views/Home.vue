5e78de857f68232c8e593c263e03b2cf21054cde
LiZiping <ziping@oriente.com>
2018-11-16 09:59:58 +0800

    Card组件提交

--- examples/views/Home.vue
+++ examples/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
-
+    <b-card :width="300" :src="src" title="Card Title" subtitle="Card subtitle" :text="text">
+      <span slot="actions">
+        <b-button>Go somewhere</b-button>
+      </span>
+    </b-card>
   </div>
 </template>
 
@@ -9,6 +13,8 @@ export default {
   name: 'Home',
   data () {
     return {
+      src: 'http://weiwuu-unity.oss-cn-beijing.aliyuncs.com/resource/46074862314501/images/57983200457235.jpg',
+      text: 'Some quick example text to build on the card title and make up the bulk of the card',
       theme: ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark']
     };
   },
@@ -28,4 +34,7 @@ export default {
   .bg-secondary, .bg-primary {
     height: 200px;
   }
+  // .text-ellipsis-2 {
+  //   width: 330px;
+  // }
 </style>
--- src/packages/card/main.vue
+++ src/packages/card/main.vue
@@ -1,32 +1,33 @@
 <template>
-  <div class="card" :class="classes">
+  <div class="card" :class="classes" :style="currentStyle">
     <slot>
       <!-- 标题 -->
       <div class="card-header" v-if="header || $slots.header">
         <slot name="header">{{ header }}</slot>
       </div>
+
       <!-- 主体 -->
       <div class="card-body">
         <!-- 图片 -->
-        <div class="card-img-top" v-if="['top', 'left'].includes(fixed) && src">
+        <div class="card-img" v-if="position === 'before' && src">
           <img :src="src" alt="">
         </div>
-        <!-- 内容 -->
         <div class="card-inner">
-          <h5 class="card-title" v-if="title">{{ title }}</h5>
-          <h6 class="card-subtitle" v-if="subtitle">{{ subtitle }}</h6>
           <slot name="body">
+            <h5 class="card-title" v-if="title">{{ title }}</h5>
+            <h6 class="card-subtitle" v-if="subtitle">{{ subtitle }}</h6>
             <p class="card-text" v-if="text">{{ text }}</p>
           </slot>
-          <div class="card-operation" v-if="$slots.actions">
-            <slot name="actions"></slot>
-          </div>
+        </div>
+        <div class="card-actions">
+          <slot name="actions"></slot>
         </div>
         <!-- 图片 -->
-        <div class="card-img-bottom" v-if="['bottom', 'right'].includes(fixed) && src">
+        <div class="card-img" v-if="position === 'after' && src">
           <img :src="src" alt="">
         </div>
       </div>
+
       <!-- 尾部 -->
       <div class="card-footer" v-if="footer || $slots.footer">
         <slot name="footer">{{ footer }}</slot>
@@ -41,28 +42,36 @@
   export default {
     name: 'BCard',
     props: {
-      mode: {
+      status: {
         type: String,
-        default: 'vertical',
-        validator: OneOf(['horizontal', 'vertical'])
+        default: 'shadow',
+        validator: OneOf(['shadow', 'border'])
       },
+      width: Number,
+      height: Number,
       header: String,
       footer: String,
       title: String,
       subtitle: String,
       text: String,
       src: String,
-      fixed: {
+      position: {
         type: String,
-        default: 'top',
-        validator: OneOf(['top', 'bottom', 'left', 'right'])
+        default: 'before',
+        validator: OneOf(['before', 'after'])
       }
     },
     computed: {
       classes () {
         return [
-          `card-${this.mode}`
+          `card-${this.status}`
         ];
+      },
+      currentStyle () {
+        return {
+          width: this.width + 'px',
+          height: this.height + 'px'
+        };
       }
     }
   };
