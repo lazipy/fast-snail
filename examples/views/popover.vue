bc320e09c08b63cc571aa7fe961a77bbf257a87d
LiZiping <ziping@oriente.com>
2018-11-22 11:36:51 +0800

    提交popover组件

--- examples/views/popover.vue
+++ examples/views/popover.vue
@@ -3,14 +3,13 @@
     <b-popover title="This is title" content="Are you sure delete it?">
       <b-button type="danger">Delete</b-button>
     </b-popover>
-    <hr>
     <b-popover title="This is title" content="Are you sure delete it?" placement="top">
       <b-button type="danger">Delete</b-button>
     </b-popover>
     <b-popover title="This is title" content="Are you sure delete it?" placement="top">
       <b-button type="danger">Delete</b-button>
     </b-popover>
-    <b-popover title="This is title" content="Are you sure delete it?" placement="right">
+    <b-popover content="Are you sure delete it?" placement="right">
       <b-button type="danger">Delete</b-button>
     </b-popover>
   </div>
