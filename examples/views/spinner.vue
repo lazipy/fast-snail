a7a6102c96bb3028a04edb3881a0d8d0f3136f01
LiZiping <ziping@oriente.com>
2018-11-25 18:40:19 +0800

    修改spinner组件

--- examples/views/spinner.vue
+++ examples/views/spinner.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container pt-20">
     <s-spinner></s-spinner>
+    <s-spinner :size="28"></s-spinner>
+    <s-spinner color="#20c997"></s-spinner>
   </div>
 </template>
 
--- src/packages/spinner/main.vue
+++ src/packages/spinner/main.vue
@@ -1,9 +1,35 @@
 <template>
-  <i class="icon-spin4 spinner"></i>
+  <i class="icon-spinner spinner" :style="currentStyle"></i>
 </template>
 
 <script>
   export default {
-    name: 'SSpinner'
+    name: 'SSpinner',
+    props: {
+      delay: {
+        type: Number,
+        default: 0
+      },
+      size: Number,
+      color: String
+    },
+    data () {
+      return {
+        visible: false
+      };
+    },
+    computed: {
+      currentStyle () {
+        return {
+          fontSize: this.size + 'px',
+          color: this.color
+        };
+      }
+    },
+    mounted () {
+      setTimeout(() => {
+        this.visible = true;
+      }, this.delay);
+    }
   };
 </script>
